346b75dcefaf556418043bd029942c2b3fd3335d
Shawn Taylor <shawn@mapsandapps.net>
2021-11-11 18:04:40 -0500

    Format name of exported file

--- code.ts
+++ code.ts
@@ -8,7 +8,7 @@ figma.ui.onmessage = message => {
 
 const selections = figma.currentPage.selection
 
-const exportNode = (node: ComponentNode) => {
+const exportNode = (node: ComponentNode, name: String) => {
   node
     .exportAsync({
       format: "SVG"
@@ -19,8 +19,40 @@ const exportNode = (node: ComponentNode) => {
       // TODO: error
     })
     .then(img => {
-      figma.ui.postMessage({ data: img, name: node.name })
+      figma.ui.postMessage({ data: img, name })
     })
+}
+
+const findComponentNode = (node: SceneNode) => {
+  if (node.type === "COMPONENT") {
+    const parentName = node.parent.name
+
+    const simplifyName = (name: String): String => {
+      return name
+        .replace(/Icon[\s-/]+/, "")
+        .replace(/([\s-/]+)/g, "-")
+        .toLowerCase()
+    }
+
+    let name
+    // @ts-ignore
+    if (node.variantProperties?.Dimensions) {
+      // @ts-ignore
+      name = `${simplifyName(parentName)}_${node.variantProperties.Dimensions}`
+    } else {
+      name = simplifyName(node.name)
+    }
+
+    exportNode(node, name)
+  } else if (node.type === "INSTANCE") {
+  } else {
+    if (node.parent) {
+      // findComponentNode(node.parent)
+      console.log(node.parent.type)
+    } else {
+      // TODO: error
+    }
+  }
 }
 
 if (selections.length > 1) {
@@ -33,9 +65,7 @@ if (selections.length > 1) {
   console.log(selectedNode)
   console.log(selectedNode.type)
 
-  if (selectedNode.type === "COMPONENT") {
-    exportNode(selectedNode)
-  }
+  findComponentNode(selectedNode)
 
   // want it to be COMPONENT
   // or (sometimes) INSTANCE
